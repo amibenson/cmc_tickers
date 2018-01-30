efece17f9fd4042aa1b76b32e308a66d1b661b2f
Ami.Benson <ami.benson@gmail.com>
2018-01-31 00:23:34 +0200

    format_day_trading_to_market_cap_percent

--- cmc_tickers/tickers/admin.py
+++ cmc_tickers/tickers/admin.py
@@ -3,7 +3,7 @@ import timeago
 from  tickers.models import *
 import datetime
 #admin.site.register(tickers.models.Ticker)
-admin.site.register(tickers.models.TickerHistory)
+admin.site.register(TickerHistory)
 
 def format_time_ago_lastUpdated(obj):
     return timeago.format(obj.lastUpdated.replace(tzinfo=None) , datetime.datetime.now().replace(tzinfo=None))
@@ -17,18 +17,18 @@ format_time_ago_dateAdded.short_description = 'date Added'
 format_time_ago_dateAdded.allow_tags = True
 format_time_ago_dateAdded.admin_order_field = 'dateAdded'
 
-"""
+
 def format_day_trading_to_market_cap_percent(obj):
     return "%d" % int((obj.dayVolumeUsd / obj.markedCapUsd) * 100)
 format_day_trading_to_market_cap_percent.short_description = 'day trading/mcap'
 format_day_trading_to_market_cap_percent.allow_tags = True
 format_day_trading_to_market_cap_percent.admin_order_field = 'dayVolumeUsd'
 
-"""
+
 
 @admin.register(Ticker)
 class TickerAdmin(admin.ModelAdmin):
-    list_display = ('rank', 'name', 'symbol', 'priceBtc', 'priceUsd', 'percentChange24h', format_time_ago_lastUpdated, format_time_ago_dateAdded)
+    list_display = ('rank', 'name', 'symbol', 'priceBtc', 'priceUsd', 'percentChange24h', format_time_ago_lastUpdated, format_time_ago_dateAdded, format_day_trading_to_market_cap_percent)
     ordering = ('rank', )
     list_filter = ('symbol',)
     search_fields = ['name', 'symbol' ]
