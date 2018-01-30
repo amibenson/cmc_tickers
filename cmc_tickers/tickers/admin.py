1affa1bf2cebf02d7fc8cdfb595517c69affc1c9
Ami.Benson <ami.benson@gmail.com>
2018-01-31 00:15:15 +0200

    format_day_trading_to_market_cap_percent

--- cmc_tickers/tickers/admin.py
+++ cmc_tickers/tickers/admin.py
@@ -19,9 +19,9 @@ format_time_ago_dateAdded.admin_order_field = 'dateAdded'
 
 def format_day_trading_to_market_cap_percent(obj):
     return obj.dayVolumeUsd / obj.markedCapUsd * 100
-format_time_ago_dateAdded.short_description = 'day trading/mcap'
-format_time_ago_dateAdded.allow_tags = True
-format_time_ago_dateAdded.admin_order_field = 'dayVolumeUsd'
+format_day_trading_to_market_cap_percent.short_description = 'day trading/mcap'
+format_day_trading_to_market_cap_percent.allow_tags = True
+format_day_trading_to_market_cap_percent.admin_order_field = 'dayVolumeUsd'
 
 
 @admin.register(Ticker)
