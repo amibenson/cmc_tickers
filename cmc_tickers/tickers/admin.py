e7072aa869d66a0ecab29e95358a5a1c12302f04
Ami.Benson <ami.benson@gmail.com>
2018-01-31 00:19:41 +0200

    format_day_trading_to_market_cap_percent

--- cmc_tickers/tickers/admin.py
+++ cmc_tickers/tickers/admin.py
@@ -17,16 +17,18 @@ format_time_ago_dateAdded.short_description = 'date Added'
 format_time_ago_dateAdded.allow_tags = True
 format_time_ago_dateAdded.admin_order_field = 'dateAdded'
 
+"""
 def format_day_trading_to_market_cap_percent(obj):
     return "%d" % int((obj.dayVolumeUsd / obj.markedCapUsd) * 100)
 format_day_trading_to_market_cap_percent.short_description = 'day trading/mcap'
 format_day_trading_to_market_cap_percent.allow_tags = True
 format_day_trading_to_market_cap_percent.admin_order_field = 'dayVolumeUsd'
 
+"""
 
 @admin.register(Ticker)
 class TickerAdmin(admin.ModelAdmin):
-    list_display = ('rank', 'name', 'symbol', 'priceBtc', 'priceUsd', 'percentChange24h', format_time_ago_lastUpdated, format_time_ago_dateAdded, format_day_trading_to_market_cap_percent)
+    list_display = ('rank', 'name', 'symbol', 'priceBtc', 'priceUsd', 'percentChange24h', format_time_ago_lastUpdated, format_time_ago_dateAdded)
     ordering = ('rank', )
     list_filter = ('symbol',)
     search_fields = ['name', 'symbol' ]
