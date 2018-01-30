4fc72b1434f0d7ab81ec80e4e2482233d81d960b
Ami.Benson <ami.benson@gmail.com>
2018-01-30 22:47:03 +0200

    add timeago

--- cmc_tickers/tickers/admin.py
+++ cmc_tickers/tickers/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-
+import timeago
 from  tickers.models import *
-
+import datetime
 #admin.site.register(tickers.models.Ticker)
 #admin.site.register(tickers.models.TickerHistory)
 
+def format_time_ago(obj):
+    return timeago.format(obj.lastUpdated , datetime.datetime.now())
+
+format_time_ago.short_description = 'last Updated'
+format_time_ago.allow_tags = True
+format_time_ago.admin_order_field = 'lastUpdated'
+
 @admin.register(Ticker)
 class TickerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'symbol',)
+    list_display = ('name', 'symbol', format_time_ago)
