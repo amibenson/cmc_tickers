22a22f14ae0643b4d0b46f02f7a074a1b07b921d
Ami.Benson <ami.benson@gmail.com>
2018-01-30 23:58:20 +0200

    tz naive now

--- cmc_tickers/tickers/admin.py
+++ cmc_tickers/tickers/admin.py
@@ -5,13 +5,21 @@ import datetime
 #admin.site.register(tickers.models.Ticker)
 #admin.site.register(tickers.models.TickerHistory)
 
-def format_time_ago(obj):
+def format_time_ago_lastUpdated(obj):
     return timeago.format(obj.lastUpdated.replace(tzinfo=None) , datetime.datetime.now().replace(tzinfo=None))
+format_time_ago_lastUpdated.short_description = 'last Updated'
+format_time_ago_lastUpdated.allow_tags = True
+format_time_ago_lastUpdated.admin_order_field = 'lastUpdated'
 
-format_time_ago.short_description = 'last Updated'
-format_time_ago.allow_tags = True
-format_time_ago.admin_order_field = 'lastUpdated'
+def format_time_ago_dateAdded(obj):
+    return timeago.format(obj.dateAdded.replace(tzinfo=None) , datetime.datetime.now().replace(tzinfo=None))
+format_time_ago_dateAdded.short_description = 'date Added'
+format_time_ago_dateAdded.allow_tags = True
+format_time_ago_dateAdded.admin_order_field = 'dateAdded'
+
 
 @admin.register(Ticker)
 class TickerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'symbol', format_time_ago)
+    list_display = ('name', 'symbol', 'priceBtc', 'priceUsd', format_time_ago_lastUpdated, format_time_ago_dateAdded)
+    ordering = ('rank', )
+    list_filter = ('symbol',)
