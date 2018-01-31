3e5f4dff6694a9c4d1bc20787eb7b95d8b18962d
Ami.Benson <ami.benson@gmail.com>
2018-01-31 11:59:14 +0200

    fix manage.py

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -1,6 +1,6 @@
 import sys
 from django.core.management.base import BaseCommand
-
+from  tickers.models import *
 
 
 class Command(BaseCommand):
@@ -8,10 +8,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         #parser.add_argument('-w', '--workers', type=int, default=1, help='number of workers.')
-        parser.add_argument('-s', '--symbol', type=str, default="HPB", help='Specific symbol name')
+        parser.add_argument('-s', '--symbol', type=str, default=None, help='Specific symbol name')
         #parser.add_argument('--workers-timeout', type=int)
 
     def handle(self, *args, **options):
         symbol    = str(options['symbol'])
-        print("Started with: %s" % symbol)
+
+        if symbol:
+            symbol = symbol.upper()
+
+        print("Started with %s" % symbol if symbol else "no specific symbol (will go over all of them)")
+
+        if symbol:
+            rs = TickerHistory.objects.filter(symbol=symbol).order_by('-lastUpdated')
+
+        if rs:
+            for reading in rs:
+                print("%s symbol at %s" % (reading.symbol, reading.lastUpdated))
+
+
+
 
