1a1b51636f66a015bc08ab24f8219d9a3b3ef148
Ami.Benson <ami.benson@gmail.com>
2018-01-31 12:14:18 +0200

    fix manage.py

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
 
         if rs:
             for reading in rs:
-                print("%s symbol at %s - %s BTC with %s%% trading percent" % (reading.symbol, get_time_ago(reading.lastUpdated), reading.priceBtc, get_day_trading_of_mcap_percent(reading)))
+                print("%s symbol at %s - %s BTC with %s%% trading percent" % (reading.symbol, get_time_ago(reading.lastUpdated), reading.priceBtc, get_day_trading_of_mcap_percent(obj=reading)))
 
 
 
