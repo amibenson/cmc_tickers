6c73c6fa57d2e8f167d21471879816187cd68da7
Ami.Benson <ami.benson@gmail.com>
2018-01-31 15:13:02 +0200

    fix volume-pat.py

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -46,10 +46,11 @@ def print_ticker_history_rs_data(rs_TickerHistory, alert_trading_volume_percent_
             print("%s symbol ticker was read %s, value %s BTC (%s%% daily change) with %s trading percent" % \
                   (reading.symbol, get_time_ago(reading.lastUpdated), reading.priceBtc, reading.percentChange24h, s_percent) \
                   )
-            if flt_max_24h_trading_volume_to_mcad_seen == None or flt_max_24h_trading_volume_to_mcad_seen > float(s_percent.replace('%', ''):
+            if flt_max_24h_trading_volume_to_mcad_seen == None or flt_max_24h_trading_volume_to_mcad_seen < float(s_percent.replace('%', '')):
                 flt_max_24h_trading_volume_to_mcad_seen = float(s_percent.replace('%', '')
 
-        if flt_max_24h_trading_volume_to_mcad_seen and float(alert_trading_volume_percent_th) < flt_max_24h_trading_volume_to_mcad_seen):
+        if flt_max_24h_trading_volume_to_mcad_seen and alert_trading_volume_percent_th and \
+           flt_max_24h_trading_volume_to_mcad_seen > alert_trading_volume_percent_th):
             print("-- ALERT %s 24h trading / mcap" % (s_percent))
 
         print("=======================\r\n")
