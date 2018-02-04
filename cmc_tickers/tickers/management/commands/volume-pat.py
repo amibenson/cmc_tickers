98a2b4473fd80794bcc0ca16920ed56e0d4394fb
Ami.Benson <ami.benson@gmail.com>
2018-02-04 11:17:56 +0200

    fix add rank;

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -44,8 +44,8 @@ def print_ticker_history_rs_data(rs_TickerHistory, alert_trading_volume_percent_
         print("=======================\r\n")
         for reading in rs:
             s_percent = get_day_trading_of_mcap_percent_for_obj(obj=reading)
-            print("%s symbol ticker was read %s, value %s BTC (%s%% daily change) with %s trading percent (MCAP: %s)" % \
-                  (reading.symbol, get_time_ago(reading.lastUpdated), reading.priceBtc, reading.percentChange24h, s_percent, format_using_humanize(reading.markedCapUsd, humanize.intword)) \
+            print("%s symbol ticker was read %s, rank #%s, value %s BTC (%s%% daily change) with %s trading percent (MCAP: %s)" % \
+                  (reading.symbol, get_time_ago(reading.lastUpdated), reading.rank, reading.priceBtc, reading.percentChange24h, s_percent, format_using_humanize(reading.markedCapUsd, humanize.intword)) \
                   )
             if flt_max_24h_trading_volume_to_mcad_seen == None or flt_max_24h_trading_volume_to_mcad_seen < float(s_percent.replace('%', '')):
                 flt_max_24h_trading_volume_to_mcad_seen = float(s_percent.replace('%', ''))
