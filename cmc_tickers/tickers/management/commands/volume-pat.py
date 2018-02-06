310cb905e6327f0a94ddc6cc5f0e87f5277dac67
Ami.Benson <ami.benson@gmail.com>
2018-02-06 23:45:29 +0200

    feature show readings percent

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -45,106 +45,106 @@ class Command(BaseCommand):
         else:
             print_ticker_history_rs_data(rs)
 
-def print_ticker_history_rs_data(rs_TickerHistory):
-    rs = rs_TickerHistory
-    if rs:
-        which_symbol = None
-        rank_seen = None
-        mcap_seen = None
-        value_seen = None
-        trading24tomcap = None
+    def print_ticker_history_rs_data(rs_TickerHistory):
+        rs = rs_TickerHistory
+        if rs:
+            which_symbol = None
+            rank_seen = None
+            mcap_seen = None
+            value_seen = None
+            trading24tomcap = None
 
-        count_available_ticker_readings = len(rs)
-        SHOW_X_TICKER_READINGS = 10
+            count_available_ticker_readings = len(rs)
+            SHOW_X_TICKER_READINGS = 10
 
-        print_reading_modulo = int(count_available_ticker_readings / SHOW_X_TICKER_READINGS)
-        print("count_available_ticker_readings: %s" % count_available_ticker_readings)
-        print("print_reading_modulo: %s" % print_reading_modulo)
+            print_reading_modulo = int(count_available_ticker_readings / SHOW_X_TICKER_READINGS)
+            print("count_available_ticker_readings: %s" % count_available_ticker_readings)
+            print("print_reading_modulo: %s" % print_reading_modulo)
 
-        flt_max_24h_trading_volume_to_mcad_seen = None
-        i_alert_rise_in_rank_count=0
-        print("=======================\r\n")
-        for indx_of_available_reading, reading in enumerate(rs):
-            s_percent = get_day_trading_of_mcap_percent_for_obj(obj=reading)
+            flt_max_24h_trading_volume_to_mcad_seen = None
+            i_alert_rise_in_rank_count=0
+            print("=======================\r\n")
+            for indx_of_available_reading, reading in enumerate(rs):
+                s_percent = get_day_trading_of_mcap_percent_for_obj(obj=reading)
 
-            current_available_reading_percent_in_available_period = int(indx_of_available_reading / 100 * count_available_ticker_readings)
-            if current_available_reading_percent_in_available_period % SHOW_X_TICKER_READINGS == 0:
-                s_displayed_percent_reading_in_period = current_available_reading_percent_in_available_period
+                current_available_reading_percent_in_available_period = int(indx_of_available_reading / 100 * count_available_ticker_readings)
+                if current_available_reading_percent_in_available_period % SHOW_X_TICKER_READINGS == 0:
+                    s_displayed_percent_reading_in_period = current_available_reading_percent_in_available_period
 
-            if indx_of_available_reading % print_reading_modulo == 0:
-                print("%s%% - %s symbol ticker was read %s, rank #%s, value %s BTC (%s%% daily change) with %s trading percent (MCAP: %s)" % \
-                      (s_displayed_percent_reading_in_period, reading.symbol, get_time_ago(reading.lastUpdated), reading.rank, reading.priceBtc, reading.percentChange24h, s_percent, format_using_humanize(reading.markedCapUsd, humanize.intword)) \
-                      )
+                if indx_of_available_reading % print_reading_modulo == 0:
+                    print("%s%% - %s symbol ticker was read %s, rank #%s, value %s BTC (%s%% daily change) with %s trading percent (MCAP: %s)" % \
+                          (s_displayed_percent_reading_in_period, reading.symbol, get_time_ago(reading.lastUpdated), reading.rank, reading.priceBtc, reading.percentChange24h, s_percent, format_using_humanize(reading.markedCapUsd, humanize.intword)) \
+                          )
 
-            if flt_max_24h_trading_volume_to_mcad_seen == None or flt_max_24h_trading_volume_to_mcad_seen < float(s_percent.replace('%', '')):
-                flt_max_24h_trading_volume_to_mcad_seen = float(s_percent.replace('%', ''))
+                if flt_max_24h_trading_volume_to_mcad_seen == None or flt_max_24h_trading_volume_to_mcad_seen < float(s_percent.replace('%', '')):
+                    flt_max_24h_trading_volume_to_mcad_seen = float(s_percent.replace('%', ''))
 
-            if not which_symbol:
-                which_symbol = reading.symbol
-            # rank
-            if not rank_seen or reading.rank > rank_seen[1] or reading.rank < rank_seen[0]:
-                if not rank_seen:
-                    rank_seen = [reading.rank , reading.rank]
-                else:
-                    if reading.rank > rank_seen[1]:
-                        rank_seen[1] = reading.rank
+                if not which_symbol:
+                    which_symbol = reading.symbol
+                # rank
+                if not rank_seen or reading.rank > rank_seen[1] or reading.rank < rank_seen[0]:
+                    if not rank_seen:
+                        rank_seen = [reading.rank , reading.rank]
+                    else:
+                        if reading.rank > rank_seen[1]:
+                            rank_seen[1] = reading.rank
 
-                    if reading.rank < rank_seen[0]:
-                        rank_seen[0] = reading.rank
+                        if reading.rank < rank_seen[0]:
+                            rank_seen[0] = reading.rank
 
-            # 24h trading / mcap
-            if not trading24tomcap or float(s_percent.replace('%', '')) > trading24tomcap[1] or float(s_percent.replace('%', '')) < trading24tomcap[0]:
-                if not trading24tomcap:
-                    trading24tomcap = [float(s_percent.replace('%', '')) , float(s_percent.replace('%', ''))]
-                else:
-                    if float(s_percent.replace('%', '')) > trading24tomcap[1]:
-                        trading24tomcap[1] = float(s_percent.replace('%', ''))
+                # 24h trading / mcap
+                if not trading24tomcap or float(s_percent.replace('%', '')) > trading24tomcap[1] or float(s_percent.replace('%', '')) < trading24tomcap[0]:
+                    if not trading24tomcap:
+                        trading24tomcap = [float(s_percent.replace('%', '')) , float(s_percent.replace('%', ''))]
+                    else:
+                        if float(s_percent.replace('%', '')) > trading24tomcap[1]:
+                            trading24tomcap[1] = float(s_percent.replace('%', ''))
 
-                    if float(s_percent.replace('%', '')) < trading24tomcap[0]:
-                        trading24tomcap[0] = float(s_percent.replace('%', ''))
+                        if float(s_percent.replace('%', '')) < trading24tomcap[0]:
+                            trading24tomcap[0] = float(s_percent.replace('%', ''))
 
-            # mcap
-            if not mcap_seen or reading.markedCapUsd > mcap_seen[1] or reading.markedCapUsd < mcap_seen[0]:
-                if not mcap_seen:
-                    mcap_seen = [reading.markedCapUsd , reading.markedCapUsd]
-                else:
-                    if reading.markedCapUsd > mcap_seen[1]:
-                        mcap_seen[1] = reading.markedCapUsd
+                # mcap
+                if not mcap_seen or reading.markedCapUsd > mcap_seen[1] or reading.markedCapUsd < mcap_seen[0]:
+                    if not mcap_seen:
+                        mcap_seen = [reading.markedCapUsd , reading.markedCapUsd]
+                    else:
+                        if reading.markedCapUsd > mcap_seen[1]:
+                            mcap_seen[1] = reading.markedCapUsd
 
-                    if reading.markedCapUsd < mcap_seen[0]:
-                        mcap_seen[0] = reading.markedCapUsd
-
-
-        if flt_max_24h_trading_volume_to_mcad_seen != None and self.alert_trading_volume_percent_th != None and \
-           flt_max_24h_trading_volume_to_mcad_seen > float(self.alert_trading_volume_percent_th):
-            print("-- ALERT %s 24h trading / mcap" % (flt_max_24h_trading_volume_to_mcad_seen))
-
-        rank_most_recent_or_now = rs[0].rank
-        rank_oldest_logged = rs[len(rs)-1].rank
-
-        s_alert_rise_in_rank = ""
-        if  rank_oldest_logged > rank_most_recent_or_now:
-            percent_rank_rise = int((rank_oldest_logged - rank_most_recent_or_now ) / rank_oldest_logged * 100)
-            if  percent_rank_rise > self.alert_rank_rise_percent_th:
-                s_alert_rise_in_rank = "%d) Hey, rank rise is %s%% from rank %s to rank %s\r\n" % (i_alert_rise_in_rank_count+1, percent_rank_rise, rank_oldest_logged, rank_most_recent_or_now)
+                        if reading.markedCapUsd < mcap_seen[0]:
+                            mcap_seen[0] = reading.markedCapUsd
 
 
-        if s_alert_rise_in_rank != "":
-            i_alert_rise_in_rank_count += 1
+            if flt_max_24h_trading_volume_to_mcad_seen != None and self.alert_trading_volume_percent_th != None and \
+               flt_max_24h_trading_volume_to_mcad_seen > float(self.alert_trading_volume_percent_th):
+                print("-- ALERT %s 24h trading / mcap" % (flt_max_24h_trading_volume_to_mcad_seen))
 
-        print("=======================\r\n"
-                "Summray for %s:\r\n"
-                "Rank: %s - %s (%s)\r\n"
-                "MCAP: %s - %s (%s)\r\n"
-                "24h Trading / MCAP: %s%% - %s%% (%s)\r\n"
-                "%s"%
-                (which_symbol,
-                 rank_seen[0], rank_seen[1], rs[0].rank,
-                 format_using_humanize(mcap_seen[0], humanize.intword), format_using_humanize(mcap_seen[1], humanize.intword), format_using_humanize(rs[0].markedCapUsd, humanize.intword),
-                 round(trading24tomcap[0],1), round(trading24tomcap[1],1), get_day_trading_of_mcap_percent_for_obj(obj=rs[0]),
-                 s_alert_rise_in_rank
-                 )
-              )
+            rank_most_recent_or_now = rs[0].rank
+            rank_oldest_logged = rs[len(rs)-1].rank
+
+            s_alert_rise_in_rank = ""
+            if  rank_oldest_logged > rank_most_recent_or_now:
+                percent_rank_rise = int((rank_oldest_logged - rank_most_recent_or_now ) / rank_oldest_logged * 100)
+                if  percent_rank_rise > self.alert_rank_rise_percent_th:
+                    s_alert_rise_in_rank = "%d) Hey, rank rise is %s%% from rank %s to rank %s\r\n" % (i_alert_rise_in_rank_count+1, percent_rank_rise, rank_oldest_logged, rank_most_recent_or_now)
+
+
+            if s_alert_rise_in_rank != "":
+                i_alert_rise_in_rank_count += 1
+
+            print("=======================\r\n"
+                    "Summray for %s:\r\n"
+                    "Rank: %s - %s (%s)\r\n"
+                    "MCAP: %s - %s (%s)\r\n"
+                    "24h Trading / MCAP: %s%% - %s%% (%s)\r\n"
+                    "%s"%
+                    (which_symbol,
+                     rank_seen[0], rank_seen[1], rs[0].rank,
+                     format_using_humanize(mcap_seen[0], humanize.intword), format_using_humanize(mcap_seen[1], humanize.intword), format_using_humanize(rs[0].markedCapUsd, humanize.intword),
+                     round(trading24tomcap[0],1), round(trading24tomcap[1],1), get_day_trading_of_mcap_percent_for_obj(obj=rs[0]),
+                     s_alert_rise_in_rank
+                     )
+                  )
 
 
 
