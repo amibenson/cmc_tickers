f5fa480ee7be8d6bb39c987ac2d02d570a6d69af
Ami.Benson <ami.benson@gmail.com>
2018-02-09 00:21:54 +0200

    1200 ticker (pick thekey and maybe cpchain)

--- cmc_tickers/tickers/utils.py
+++ cmc_tickers/tickers/utils.py
@@ -15,7 +15,7 @@ def get_day_trading_of_mcap_percent_for_obj(obj):
     return get_day_trading_of_mcap_percent(obj.dayVolumeUsd, obj.markedCapUsd)
 
 def get_day_trading_of_mcap_percent(dayVolumeUsd, markedCapUsd):
-    if markedCapUsd:
+    if markedCapUsd and dayVolumeUsd:
         return '{0:.1f}%'.format(dayVolumeUsd / markedCapUsd * 100)
     else:
         return None
