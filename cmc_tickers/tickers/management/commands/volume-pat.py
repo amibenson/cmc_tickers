f0909b85ca35cd20f3794ed61b72b4a8501336bf
Ami.Benson <ami.benson@gmail.com>
2018-02-09 15:47:33 +0200

    1200 ticker (pick thekey and maybe cpchain)

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -69,6 +69,10 @@ class Command(BaseCommand):
             print("=======================\r\n")
             for indx_of_available_reading, reading in enumerate(rs):
                 s_percent = get_day_trading_of_mcap_percent_for_obj(obj=reading)
+                if s_percent != None:
+                    fl_percent = float(s_percent.replace('%', ''))
+                else:
+                    fl_percent = None
 
                 current_available_reading_percent_in_available_period = int(indx_of_available_reading / 100 * count_available_ticker_readings)
                 if current_available_reading_percent_in_available_period % SHOW_X_TICKER_READINGS == 0:
@@ -79,8 +83,8 @@ class Command(BaseCommand):
                           (s_displayed_percent_reading_in_period, reading.symbol, get_time_ago(reading.lastUpdated), reading.rank, reading.priceBtc, reading.percentChange24h, s_percent, format_using_humanize(reading.markedCapUsd, humanize.intword)) \
                           )
 
-                if (flt_max_24h_trading_volume_to_mcad_seen == None and s_percent != None) or flt_max_24h_trading_volume_to_mcad_seen < float(s_percent.replace('%', '')):
-                    flt_max_24h_trading_volume_to_mcad_seen = float(s_percent.replace('%', ''))
+                if flt_max_24h_trading_volume_to_mcad_seen == None or flt_max_24h_trading_volume_to_mcad_seen < fl_percent):
+                    flt_max_24h_trading_volume_to_mcad_seen = fl_percent
 
                 if not which_symbol:
                     which_symbol = reading.symbol
@@ -96,15 +100,15 @@ class Command(BaseCommand):
                             rank_seen[0] = reading.rank
 
                 # 24h trading / mcap
-                if not trading24tomcap or float(s_percent.replace('%', '')) > trading24tomcap[1] or float(s_percent.replace('%', '')) < trading24tomcap[0]:
+                if not trading24tomcap or fl_percent > trading24tomcap[1] or fl_percent < trading24tomcap[0]:
                     if not trading24tomcap:
-                        trading24tomcap = [float(s_percent.replace('%', '')) , float(s_percent.replace('%', ''))]
+                        trading24tomcap = [fl_percent , fl_percent]
                     else:
-                        if float(s_percent.replace('%', '')) > trading24tomcap[1]:
-                            trading24tomcap[1] = float(s_percent.replace('%', ''))
+                        if fl_percent > trading24tomcap[1]:
+                            trading24tomcap[1] = fl_percent
 
-                        if float(s_percent.replace('%', '')) < trading24tomcap[0]:
-                            trading24tomcap[0] = float(s_percent.replace('%', ''))
+                        if fl_percent < trading24tomcap[0]:
+                            trading24tomcap[0] = fl_percent
 
                 # mcap
                 if not mcap_seen or reading.markedCapUsd > mcap_seen[1] or reading.markedCapUsd < mcap_seen[0]:
