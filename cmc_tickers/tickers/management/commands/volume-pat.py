690667eaff2c7c70569185b0b8d6004edbd292b2
Ami.Benson <ami.benson@gmail.com>
2018-02-12 09:32:11 +0200

    fixes

--- cmc_tickers/tickers/management/commands/volume-pat.py
+++ cmc_tickers/tickers/management/commands/volume-pat.py
@@ -76,7 +76,7 @@ class Command(BaseCommand):
                     fl_percent = None
 
                 current_available_reading_percent_in_available_period = int((indx_of_available_reading / count_available_ticker_readings) * 100)
-                if current_available_reading_percent_in_available_period % SHOW_X_TICKER_READINGS == 0 :
+                if current_available_reading_percent_in_available_period % SHOW_X_TICKER_READINGS == 0 or indx_of_available_reading+1 == len(rs):
                     s_displayed_percent_reading_in_period = current_available_reading_percent_in_available_period
 
                 # Print ticker if last ticker read (oldest one) or if we reached far enough from previous printed ticker
